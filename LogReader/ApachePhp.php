7f7bc134af8dc97eefa3f8956919dd4d75f8dd85
rnix <rnix@yandex.ru>
2014-04-02 17:49:35 +0700

    Fixed apache error types

--- LogReader/ApachePhp.php
+++ LogReader/ApachePhp.php
@@ -1,39 +1,44 @@
 <?php
 
 class LogReader_ApachePhp extends LogReader_Abstract {
-    
-    
+
     public function read() {
         $item = new LogReader_Item_ApachePhp();
         while (!$this->_file->eof()) {
-            
-            if (preg_match('/^\[(.+?)\] \[(.+?)\] \[(.+?)\] (PHP )?(.+), referer: (.+)/', $this->_file->fgets(), $matches)) {
-                list ($raw, $date, $error, $client, $php, $message, $referer) = $matches;
-                
+
+            if (preg_match('/^\[(?<date>.+?)\] \[(?:.+?)\] \[(?<client>.+?)\] (?<php_type>PHP)?(?<message>.+?)(, referer: (?<referer>.+))?$/', $this->_file->fgets(), $matches)) {
+                $date = $matches['date'];
+                $message = $matches['message'];
+
                 if (preg_match('/^(Stack trace|[\d])/', trim($message))) {
                     //this line is part of stack trace
                     $item->appendStackTrace($message);
                 } else {
                     $timestamp = date('Y-m-d H:i:s', strtotime($date));
                     $item->setTimestamp($timestamp);
-                    $type = $this->_getType($message);
+                    if (!empty($matches['php_type'])) {
+                        $type = $this->_getType($message);
+                    } else {
+                        $type = 'Apache';
+                    }
                     $item->setType($type);
-                    $item->setReferer($referer);
+                    if (isset($matches['referer'])) {
+                        $item->setReferer($matches['referer']);
+                    }
                     $stackTrace = $item->getStackTrace();
                     $messagesArray = array_merge(array($message), $stackTrace);
                     $item->setMessage(implode("\n", $messagesArray));
-                    
+
                     if ($this->_storage) {
                         $this->_storage->save($item);
                     }
-                    
+
                     $item = new LogReader_Item_ApachePhp();
                 }
             }
-            
         }
     }
-    
+
     protected function _getType($message) {
         if (preg_match('/^([a-zA-Z0-9 ]+): /', $message, $matches) && isset($matches[1])) {
             return trim($matches[1]);
