60b84446be33be3aa4cd5320f6a480d8ae66eb8c
rnix <rnix@yandex.ru>
2014-04-03 09:31:26 +0700

    Fixed php stack trace

--- LogReader/ApachePhp.php
+++ LogReader/ApachePhp.php
@@ -9,11 +9,15 @@ class LogReader_ApachePhp extends LogReader_Abstract {
             if (preg_match('/^\[(?<date>.+?)\] \[(?:.+?)\] \[(?<client>.+?)\] (?<php_type>PHP)?(?<message>.+?)(, referer: (?<referer>.+))?$/', $this->_file->fgets(), $matches)) {
                 $date = $matches['date'];
                 $message = $matches['message'];
-
+                
                 if (preg_match('/^(Stack trace|[\d])/', trim($message))) {
                     //this line is part of stack trace
                     $item->appendStackTrace($message);
                 } else {
+                    $this->_save($item);
+                    
+                    $item = new LogReader_Item_ApachePhp();
+                    
                     $timestamp = date('Y-m-d H:i:s', strtotime($date));
                     $item->setTimestamp($timestamp);
                     if (!empty($matches['php_type'])) {
@@ -25,23 +29,26 @@ class LogReader_ApachePhp extends LogReader_Abstract {
                     if (isset($matches['referer'])) {
                         $item->setReferer($matches['referer']);
                     }
-                    $stackTrace = $item->getStackTrace();
-                    $messagesArray = array_merge(array($message), $stackTrace);
-                    $item->setMessage(implode("\n", $messagesArray));
-
-                    if ($this->_storage) {
-                        $this->_storage->save($item);
-                    }
-
-                    $item = new LogReader_Item_ApachePhp();
+                    $item->setMessage($message);    
                 }
             }
         }
+        
+        $this->_save($item);
     }
 
     protected function _getType($message) {
         if (preg_match('/^([a-zA-Z0-9 ]+): /', $message, $matches) && isset($matches[1])) {
             return trim($matches[1]);
+        }
+    }
+    
+    protected function _save(LogReader_Item_ApachePhp $item) {
+        if ($item->getMessage() && $this->_storage) {
+            $stackTrace = $item->getStackTrace();
+            $messagesArray = array_merge(array($item->getMessage()), $stackTrace);
+            $item->setMessage(implode("\n", $messagesArray));
+            $this->_storage->save($item);
         }
     }
 
